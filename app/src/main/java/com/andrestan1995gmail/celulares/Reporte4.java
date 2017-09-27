88d1d30fa7732b4f1ad669afbc74c226ffa32653
AndresEstan <andrestan1995@gmail.com>
2017-09-26 23:52:28 -0500

    funciona, faltan las validaciones

--- app/src/main/java/com/andrestan1995gmail/celulares/Reporte4.java
+++ app/src/main/java/com/andrestan1995gmail/celulares/Reporte4.java
@@ -2,12 +2,29 @@ package com.andrestan1995gmail.celulares;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Reporte4 extends AppCompatActivity {
-
+    private TextView tv;
+    private ArrayList<Celular> celular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte4);
+        tv = (TextView) findViewById(R.id.txtCantidad);
+        int cont=0;
+        celular = Datos.obtener();
+
+        for (int i = 0; i <celular.size(); i++){
+
+            if(celular.get(i).getMarca().equalsIgnoreCase("apple")&&celular.get(i).getColor().equalsIgnoreCase("negro")){
+                cont=cont+1;
+            }
+        }
+        String result=String.valueOf(cont);
+        tv.setText(result);
     }
 }
+
--- app/src/main/java/com/andrestan1995gmail/celulares/Reporte5.java
+++ app/src/main/java/com/andrestan1995gmail/celulares/Reporte5.java
@@ -2,12 +2,40 @@ package com.andrestan1995gmail.celulares;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Reporte5 extends AppCompatActivity {
-
+    private TextView tv;
+    private ArrayList<Celular> celular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte5);
+        tv = (TextView) findViewById(R.id.txtPromedio);
+        int suma=0, cont=0, promedio=0;
+
+
+
+        celular = Datos.obtener();
+
+        for (int i = 0; i <celular.size(); i++){
+
+            if(celular.get(i).getMarca().equalsIgnoreCase("nokia")){
+                suma=suma+Integer.parseInt(celular.get(i).getPrecio());
+                cont=cont+1;
+            }
+
+
+        }
+
+        promedio=suma/cont;
+        String result=String.valueOf(promedio);
+        tv.setText("$"+result);
+
     }
 }
