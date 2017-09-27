d6b5601a85d28240a41de68b41e305364f2b3386
AndresEstan <andrestan1995@gmail.com>
2017-09-26 19:36:00 -0500

    Interfaz grafica

---
+++ app/src/main/java/com/andrestan1995gmail/celulares/Celular.java
@@ -0,0 +1,8 @@
+package com.andrestan1995gmail.celulares;
+
+/**
+ * Created by android on 26/09/2017.
+ */
+
+public class Celular {
+}
---
+++ app/src/main/java/com/andrestan1995gmail/celulares/ListarCelulares.java
@@ -0,0 +1,13 @@
+package com.andrestan1995gmail.celulares;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListarCelulares extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_celulares);
+    }
+}
